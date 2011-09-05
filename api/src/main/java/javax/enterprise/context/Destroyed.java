b2dea100087bf5c483b416e82f351c552f2cfbec
Pete Muir <pmuir@bleepbleep.org.uk>
2011-09-05 19:48:43 +0100

    CDI-86, add application lifecycle events
    * New Events
    
      * @Initialized(ApplicationScoped.class) Object o;
      * @Destroyed(ApplicationScoped.class) Object o;
      * @Initialized(SessionScoped.class) Object o;
      * @Destroyed(SessionScoped.class) Object o;
      * @Initialized(ConversationScoped.class) Object o;
      * @Destroyed(ConversationScoped.class) Object o;
      * @Initialized(RequestScoped.class) Object o;
      * @Destroyed(RequestScoped.class) Object o;
    
    * Add these open issues
    
      * The CDI 1.0 spec doesn't define when a context
        is "initialized" (however the concept is
        implied), however the CDI spec introduces
        lifecycle events that require an event to be
        fired when a context is initialized. Either we
        need to rewrite the definition of this feature
        to use the CDI 1.0 language or we need to
        define when a context is initialized.
      * What is the event type when the application is
        not a web application?

---
+++ api/src/main/java/javax/enterprise/context/Destroyed.java
@@ -0,0 +1,35 @@
+package javax.enterprise.context;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * <p>
+ * The <code>@Destroyed</code> qualifier.
+ * </p>
+ * 
+ * @author Pete Muir
+ * @see Destroyed
+ */
+@Qualifier
+@Target({ TYPE, METHOD, PARAMETER, FIELD })
+@Retention(RUNTIME)
+@Documented
+public @interface Destroyed {
+   
+   /**
+    * The scope for which to observe initialization
+    */
+   Class<? extends Annotation> value();
+
+}
