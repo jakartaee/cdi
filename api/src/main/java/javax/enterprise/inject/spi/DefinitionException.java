c3a950335fcb14de7f9da4af6a7a646fce10de5d
Pete Muir <pmuir@bleepbleep.org.uk>
2011-09-07 19:56:27 -0400

    CDI-118, standardize deployment exceptions

---
+++ api/src/main/java/javax/enterprise/inject/spi/DefinitionException.java
@@ -0,0 +1,41 @@
+package javax.enterprise.inject.spi;
+
+/**
+ * <p>
+ * Thrown when a definition error occurs.
+ * </p>
+ * 
+ * <p>
+ * Definition errors are developer errors. They may be detected by tooling at development time, and
+ * are also detected by the container at initialization time. If a definition error exists in a
+ * deployment, initialization will be aborted by the container.
+ * </p>
+ * 
+ * <p>
+ * The container is permitted to define a non-portable mode, for use at development time, in which
+ * some definition errors do not cause application initialization to abort.
+ * </p>
+ * 
+ * <p>
+ * An implementation is permitted to throw a subclass of {@link DefinitionException} for any
+ * definition error which exists.
+ * </p>
+ * 
+ * @author Pete Muir
+ * 
+ */
+public class DefinitionException extends RuntimeException {
+
+   public DefinitionException(String message, Throwable t) {
+      super(message, t);
+   }
+
+   public DefinitionException(String message) {
+      super(message);
+   }
+
+   public DefinitionException(Throwable t) {
+      super(t);
+   }
+
+}
