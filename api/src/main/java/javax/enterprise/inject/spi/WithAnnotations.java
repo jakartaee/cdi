9522c8660dbffdf54f750b5c7dee91681154d33a
Pete Muir <pmuir@bleepbleep.org.uk>
2012-08-21 20:24:22 +0100

    CDI-43 Provide @WithAnnotations to filter which container lifecycle events are called.

---
+++ api/src/main/java/javax/enterprise/inject/spi/WithAnnotations.java
@@ -0,0 +1,31 @@
+package javax.enterprise.inject.spi;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * {@link WithAnnotations} may be applied to any portable extension observer method with an event
+ * parameter type of {@link ProcessAnnotatedType} to filter the events delivered.
+ * </p>
+ * 
+ * <p>
+ * If the {@link WithAnnotations} annotation is applied to a portable extension observer method,
+ * then only {@link ProcessAnnotatedType} events for types which have at least one of the
+ * annotations specified. The annotation can appear on the type, on any field, method or construtor
+ * declared by the type, or on any parameter of any method or constructor declared by the type
+ * </p>
+ * 
+ * @author Pete Muir
+ * 
+ */
+@Retention(RUNTIME)
+@Target(PARAMETER)
+public @interface WithAnnotations {
+
+   Class<?>[] value();
+
+}
