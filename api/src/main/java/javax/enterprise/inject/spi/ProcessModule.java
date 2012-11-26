429c1f09f9f7e1b2f583529afba789d008b24297
Pete Muir <pmuir@bleepbleep.org.uk>
2012-11-26 16:44:09 +0000

    CDI-295 fix set -> list

--- api/src/main/java/javax/enterprise/inject/spi/ProcessModule.java
+++ api/src/main/java/javax/enterprise/inject/spi/ProcessModule.java
@@ -1,19 +1,17 @@
 package javax.enterprise.inject.spi;
 
 import java.io.InputStream;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 /**
  * <p>
  * The container fires an event of this type for each, before it processes the classes packaged in that module.
  * </p>
  * <p>
- * Any observer of this event is permitted to add classes to, or remove classes from, the set of alternatives, list of
- * interceptors or list of decorators. The container will use the final values of these collections, after all observers have
+ * Any observer of this event is permitted to add classes to, or remove classes from, the list of alternatives, list of
+ * interceptors or list of decorators. The container will use the final values of these lists, after all observers have
  * been called, to determine the enabled alternatives, interceptors, and decorators for the bean deployment archive. The initial
- * values of these collections is determined by reading the <code>beans.xml</code> file of the bean deployment archive.
+ * values of these lists is determined by reading the <code>beans.xml</code> file of the bean deployment archive.
  * </p>
  * <p>
  * If any observer method of a {@code ProcessModule} event throws an exception, the exception is treated as a definition error
@@ -25,9 +23,9 @@ import java.util.Set;
 public interface ProcessModule {
 
     /**
-     * @return the set of enabled alternatives of the bean deployment archive
+     * @return the list of enabled alternatives of the bean deployment archive
      */
-    public Set<Class<?>> getAlternatives();
+    public List<Class<?>> getAlternatives();
 
     /**
      * @return the list of enabled interceptors of the bean deployment archive.
