37787b281fe8f285d81cd036b1725ef8771ddbcb
Tomas Remes <tremes@redhat.com>
2015-02-04 15:36:59 +0100

    Introduce tck assertion mapper integration

---
+++ tck-mapper/src/main/java/org/jboss/cdi/tck/audit/SectionElement.java
@@ -0,0 +1,17 @@
+package org.jboss.cdi.tck.audit;
+
+/**
+ * @author Tomas Remes
+ */
+public class SectionElement {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
