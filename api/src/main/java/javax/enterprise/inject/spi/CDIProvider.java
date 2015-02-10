a112489f248ab9074da4d0a81a28abc67f8cdbe5
John D. Ament <john.ament@spartasystems.com>
2015-02-10 12:57:39 -0500

    CDI-26 Files not needed. (+8 squashed commits) Squashed commits: [d005b5a] CDI-26 Cleaned up docs after EG review. [4a86f00] CDI-26 Simplified try with resources example. [e248ce4] CDI-26 Added docs for SE. [a731d53] CDI-26 Added docs for SE. [8d1c9d4] CDI-26 Close shouldn't have been throwing an exception. [dd81e7a] CDI-26 Minor javadoc clean up. [3115343] CDI-509 Updating pom file to use latest felix plugin. [9db0748] CDI-26 Bootstrap API for CDI.

--- api/src/main/java/javax/enterprise/inject/spi/CDIProvider.java
+++ api/src/main/java/javax/enterprise/inject/spi/CDIProvider.java
@@ -1,5 +1,8 @@
 package javax.enterprise.inject.spi;
 
+import java.util.Collections;
+import java.util.Map;
+
 /**
  * Interface implemented by a CDI provider to provide access to the current container
  * 
@@ -14,5 +17,32 @@ public interface CDIProvider {
      * @return the CDI instance for the current container
      */
     public CDI<Object> getCDI();
+
+    /**
+     * Determines whether or not this CDIProvider has been initialized or not.
+     * @return true if initialized, false if not.
+     */
+    boolean isInitialized();
+
+    /**
+     * Initializes a CDI Container.  Cannot be called within an application server or if the container is already started.
+     * If called in either of these ways, an {@link java.lang.IllegalStateException} is thrown.
+     *
+     * @return the {@link CDI} instance associated with the container.  This is the same instance returned by using
+     * {@link CDI.current()}
+     */
+    default CDI<Object> initialize() {
+        return initialize(Collections.EMPTY_MAP);
+    }
+
+    /**
+     * Initializes a CDI Container.  Cannot be called within an application server or if the container is already started.
+     * If called in either of these ways, an {@link java.lang.IllegalStateException} is thrown.
+     *
+     * @param params optional parameters, may be null or empty.  May also be immutable.
+     * @return the {@link CDI} instance associated with the container.  This is the same instance returned by using
+     * {@link CDI.current()}
+     */
+    CDI<Object> initialize(Map<String,Object> params);
 
 }
