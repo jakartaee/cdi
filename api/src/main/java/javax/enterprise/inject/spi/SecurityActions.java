676366d034beba61a96a5a3c5108c7e9bd5fe09e
Antoine Sabot-Durand <antoine@sabot-durand.net>
2018-07-19 16:31:02 +0200

    Prepare for 2.0.SP1 release

--- api/src/main/java/javax/enterprise/inject/spi/SecurityActions.java
+++ api/src/main/java/javax/enterprise/inject/spi/SecurityActions.java
@@ -4,10 +4,10 @@
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
- * Licensed under the Apache License, Version 2.1-SNAPSHOT (the "License");
+ * Licensed under the Apache License, Version 2.0.SP1 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.1-SNAPSHOT
+ * http://www.apache.org/licenses/LICENSE-2.0.SP1
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
