ffbea9431c05666a07584c7a8bc313c92267edad
Antoine Sabot-Durand <antoine@sabot-durand.net>
2019-04-16 11:25:31 +0200

    Update to secure https access

--- api/src/main/java/javax/enterprise/inject/spi/WithAnnotations.java
+++ api/src/main/java/javax/enterprise/inject/spi/WithAnnotations.java
@@ -34,7 +34,7 @@ import java.lang.annotation.Target;
  * If the {@link WithAnnotations} annotation is applied to a portable extension observer method, then only
  * {@link ProcessAnnotatedType} events for types which have at least one of the annotations specified are observed. The
  * annotation can appear on the annotated type, or on any member, or any parameter of any member of the annotated type, as defined 
- * in section <a href="http://docs.jboss.org/cdi/spec/1.2/cdi-spec.html#alternative_metadata_sources">11.4 Alternative metadata sources</a>. 
+ * in section <a href="https://docs.jboss.org/cdi/spec/1.2/cdi-spec.html#alternative_metadata_sources">11.4 Alternative metadata sources</a>.
  * The annotation may be applied as a meta-annotation on any annotation considered.
  *
  * @author Pete Muir
