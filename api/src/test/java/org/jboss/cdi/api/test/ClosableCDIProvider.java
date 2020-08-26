a5a2d678476c1b932f7935e4403c8c2c0e89ff69
Doychin Denkov Bondzhev <doychin@dsoft-bg.com>
2020-08-26 17:47:00 +0300

    Check configuredProvider that it holds valid CDI container. (#445)
    Check for both IllegalStateException and null value when filtering discovered providers.
    
    Signed-off-by: Doychin Bondzhev <doychin@dsoft-bg.com>

---
+++ api/src/test/java/org/jboss/cdi/api/test/ClosableCDIProvider.java
@@ -0,0 +1,96 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.cdi.api.test;
+
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.*;
+import javax.enterprise.util.TypeLiteral;
+import java.lang.annotation.Annotation;
+import java.util.Iterator;
+
+/**
+ * Created by antoine on 16/12/2015.
+ */
+public class ClosableCDIProvider implements CDIProvider {
+
+    private static CDI<Object> cdiInstance = new DummyCDI();
+
+    static void closeContainer() {
+        cdiInstance = null;
+    }
+
+    @Override
+    public CDI<Object> getCDI() {
+        return cdiInstance;
+    }
+
+    @Override
+    public int getPriority() {
+        return 20;
+    }
+
+    public static class DummyCDI extends CDI<Object> {
+
+
+        @Override
+        public BeanManager getBeanManager() {
+            return null;
+        }
+
+        @Override
+        public Instance<Object> select(Annotation... qualifiers) {
+            return null;
+        }
+
+        @Override
+        public <U> Instance<U> select(Class<U> subtype, Annotation... qualifiers) {
+            return null;
+        }
+
+        @Override
+        public <U> Instance<U> select(TypeLiteral<U> subtype, Annotation... qualifiers) {
+            return null;
+        }
+
+        @Override
+        public boolean isUnsatisfied() {
+            return false;
+        }
+
+        @Override
+        public boolean isAmbiguous() {
+            return false;
+        }
+        
+        @Override
+        public void destroy(Object instance) {
+
+        }
+
+        @Override
+        public Iterator<Object> iterator() {
+            return null;
+        }
+
+        @Override
+        public Object get() {
+            return null;
+        }
+    }
+
+}
