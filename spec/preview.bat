6b86c0e30500700951c8bea6c3cf2b331dbea584
starksm64 <starksm64@gmail.com>
2021-12-10 12:00:59 -0600

    Remove obsolete ruby and web brower plugin stuff, fix reference to JCP. Update cdi-api to use latested released and staged apis in dependencies Update the major changes for 4.0 in preface
    Signed-off-by: starksm64 <starksm64@gmail.com>

--- spec/preview.bat
+++
@@ -1,3 +0,0 @@
-asciidoctor -o target\cdi-spec.html src\main\doc\cdi-spec.asciidoc
-
-bundle exec guard
--- spec/preview.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-asciidoctor -o target/cdi-spec.html src/main/doc/cdi-spec.asciidoc
-
-bundle exec guard
