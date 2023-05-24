a4190e0f9bf69fa2d904a245b4c19b1f6e387ea7
Ladislav Thon <lthon@redhat.com>
2023-05-24 12:37:15 +0200

    Place the EL integration API to ELAwareBeanManager in a new supplemental API artifact
    The existing EL integration API in `BeanManager` is deprecated for removal.

---
+++ el/src/main/java/jakarta/enterprise/inject/spi/el/ELAwareBeanManager.java
@@ -0,0 +1,34 @@
+package jakarta.enterprise.inject.spi.el;
+
+import jakarta.el.ELResolver;
+import jakarta.el.ExpressionFactory;
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.inject.spi.BeanManager;
+
+/**
+ * A {@link BeanManager} that allows integrators to obtain Unified EL objects
+ * that are integrated with the CDI container as described in the CDI specification.
+ *
+ * @since 4.1
+ */
+public interface ELAwareBeanManager extends BeanManager {
+    /**
+     * Returns a {@link jakarta.el.ELResolver} that resolves beans by EL name.
+     *
+     * @return the {@link jakarta.el.ELResolver}
+     */
+    public ELResolver getELResolver();
+
+    /**
+     * Returns a wrapper {@link jakarta.el.ExpressionFactory} that delegates {@link jakarta.el.MethodExpression} and
+     * {@link jakarta.el.ValueExpression} creation to the given {@link jakarta.el.ExpressionFactory}. When a Unified EL expression
+     * is evaluated using a {@link jakarta.el.MethodExpression} or {@link jakarta.el.ValueExpression} returned by the wrapper
+     * {@link jakarta.el.ExpressionFactory}, the container handles destruction of objects with scope
+     * {@link Dependent}.
+     *
+     * @param expressionFactory the {@link jakarta.el.ExpressionFactory} to wrap
+     * @return the wrapped {@link jakarta.el.ExpressionFactory}
+     */
+    public ExpressionFactory wrapExpressionFactory(ExpressionFactory expressionFactory);
+
+}
