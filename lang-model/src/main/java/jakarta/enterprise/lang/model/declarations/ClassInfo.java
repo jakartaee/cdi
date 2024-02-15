c9f31796bbc08819656db832375fabf104671e6a
Andrew Rouse <anrouse@uk.ibm.com>
2024-02-15 16:28:43 +0000

    Bring copyright headers into agreement with NOTICE (#762)
    * Bring copyright headers into agreement with NOTICE
    
    Remove the references to the use of the @authors tag and copyright.txt
    as a record of contributors.
    
    NOTICE.md states that the source code repository logs should be used to
    determine authorship.
    
    * Remove one stray "home of open source" line
    
    Most of these were removed in a previous PR

--- lang-model/src/main/java/jakarta/enterprise/lang/model/declarations/ClassInfo.java
+++ lang-model/src/main/java/jakarta/enterprise/lang/model/declarations/ClassInfo.java
@@ -1,7 +1,5 @@
 /*
  * Copyright 2021, Red Hat, Inc., and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
